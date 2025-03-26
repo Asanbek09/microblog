c0616b0c287716b01fc2f9d02824398df1b62dec
Asanbek09 <samudinua@gmail.com>
2025-03-26 10:19:42 +0600

    Работа над аватаром пользователя

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
@@ -19,6 +20,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon={}'.format(digest, size)
 
 @login.user_loader
 def load_user(id):
