1cf3e2c6268d42020cccf3bac10dedd6161d3c0c
Asanbek09 <samudinua@gmail.com>
2025-04-01 17:13:41 +0600

    Работа с датой

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import logging
+import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
