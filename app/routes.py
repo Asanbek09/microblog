133652736a7b1096896054d134b7819f2751e197
Asanbek09 <samudinua@gmail.com>
2025-04-02 14:49:10 +0600

    translate.py + небольшие изменения

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from werkzeug.urls import url_parse
 from flask_login import login_required
 from datetime import datetime
 from flask_babel import get_locale
+from guess_language import guess_language
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -15,7 +16,10 @@ from flask_babel import get_locale
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
