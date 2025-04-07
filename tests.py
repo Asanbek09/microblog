cd8d2c02f20d693c0b67addb44eac95a05beb7a2
Asanbek09 <samudinua@gmail.com>
2025-04-07 14:31:45 +0600

    Небольшие изменения по проекту

--- tests.py
+++ tests.py
@@ -1,19 +1,24 @@
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
+from config import Config
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        with app.app_context():
-            db.create_all()
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
 
     def tearDown(self):
-        with app.app_context():
-            db.session.remove()
-            db.drop_all()
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User(username='susan')
